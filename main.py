cdf5a1b80bb5776023ff0545322c08867dd10213
danbenami <danbenami3@gmail.com>
2022-12-16 01:28:36 +0200

    tasks 2,5

--- main.py
+++ main.py
@@ -4,8 +4,30 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
+def normalize_col(df, col_name, scaled_flag=True):
+    if scaled_flag:
+        df[col_name] = df[col_name] / max(df[col_name])
+    else:
+        mean = np.mean(df[col_name])
+        std = np.std(df[col_name])
+        df[col_name] = (df[col_name] - mean) / std
+
+
+
+
+# make all the columns be represented when printing
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+# read df
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
+print(train.head())
+print(train.describe())
+
+# data processing
+train.Status = [1 if stat=="Developed" else 0 for stat in train.Status] # make Status binary, Developed=1, Developing=0
+normalize_col(train,"Adult Mortality")
 
 train.drop(['Year'], axis=1, inplace=True)
 train.drop([' thinness 5-9 years'], axis=1, inplace=True)
